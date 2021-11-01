1eaee7c86afd18613ac9a745663ed43790188b58
jasondavis87 <jasondavis87@gmail.com>
2021-11-01 00:08:20 -0400

    Update install_mt5.sh

--- src/install_mt5.sh
+++ src/install_mt5.sh
@@ -1,12 +1,16 @@
 wine mt5setup.exe &
 
 sleep 10
-
+xdotool key Tab
+sleep 1
+xdotool key Up
+sleep 1
 xdotool key Return
+
 
 echo 'initializing mt5 install'
 
-export COMMON='/headless/.wine/drive_c/Program Files/MetaTrader 5/Config/common.ini'
+export COMMON='/headless/.wine/drive_c/Program Files/MetaTrader 5 Strategy Tester/Tester/Manager/common.ini'
 while :
 do
   if test -f "$COMMON"
